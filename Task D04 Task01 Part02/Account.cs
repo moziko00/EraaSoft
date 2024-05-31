5078b2ca1aeb7608fdf8029a5a8a6d76dedd0111
Mohamed Zakaria <132154144+moziko00@users.noreply.github.com>
2024-05-31 20:07:49 +0300

    Task

---
+++ Task D04 Task01 Part02/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_D04_Part02
+{
+    public class Account
+    {
+        private decimal _balance;
+        private string _name;
+
+        public Account(string name, decimal balane)
+        {
+            _balance = balane;
+            _name = name;
+        }
+
+        public bool Deposite(decimal amountToDeposite)
+        {
+            if (amountToDeposite > 0)
+            {
+                _balance += amountToDeposite;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Withdraw(decimal amountToDraw)
+        {
+            if (_balance > 0)
+            {
+                _balance -= amountToDraw;
+                return true;
+            }
+            return false;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"The Name is {_name}and The Blance is {_balance}");
+        }
+
+
+    }
+}
---
+++ Task D04 Task01 Part02/Program.cs
@@ -0,0 +1,63 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task_D04_Part02
+{
+    internal class Program
+    {
+        public enum MenuOption
+        {
+            Withdraw = 0,
+            Deposite = 1,
+            Print = 2,
+            Quit = 3
+        }
+        static void Main(string[] args)
+        {
+
+
+        }
+        public static int GetUserSelection()
+        {
+            bool isValidSelection = false;
+            int userSelection = 0;
+
+            do
+            {
+                Console.WriteLine("Please select an option (1-4):");
+                string input = Console.ReadLine();
+
+                // Attempt to convert the input to an integer
+                if (int.TryParse(input, out int selection))
+                {
+                    // Check if the selection is within the valid range
+                    if (selection >= 1 && selection <= 4)
+                    {
+                        userSelection = selection;
+                        isValidSelection = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid selection. Please enter a number between 1 and 4.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+
+            } while (!isValidSelection);
+
+            return userSelection;
+        }
+
+        private static void DoDeposite(Account account)
+        {
+            switch (account)
+            {
+                case MenuOption.Deposite:
+                    DoDeposite(account);
+                    break;
+            }
+        }
+    }
+}
