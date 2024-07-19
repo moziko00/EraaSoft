c242de9492ba5ebd7850ef80800560e9787f2fb9
Mohamed Zakaria <132154144+moziko00@users.noreply.github.com>
2024-07-19 23:54:21 +0300

    EF-Task02-ML

---
+++ EF-Core-Task 02/ML/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02_EF.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=BikeStores;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
---
+++ EF-Core-Task 02/ML/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task_02_EF.Data;
+
+namespace Task_02_EF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BikeStoresContext context = new BikeStoresContext();
+
+            var categoiries= context.Categories.ToList();
+
+
+            var firstProduct = context.Products.FirstOrDefault();
+
+            int productId = 1;
+            var specificProduct = context.Products.Find(productId);
+
+
+            // Retrieve products with a certain model year
+            int modelYear = 2023;
+            var productsByYear = context.Products.Where(p => p.ModelYear == modelYear).ToList();
+
+
+
+            // Retrieve a specific customer by ID
+            int customerId = 1;
+            var specificCustomer = context.Customers.Find(customerId);
+
+
+
+        }
+    }
+}
